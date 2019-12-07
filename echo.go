c22ebbc12fcb32c22491c097e077b35a627a6f0e
winlin <winlin@vip.126.com>
2019-12-07 09:05:37 +0800

    Update

--- echo.go
+++ echo.go
@@ -20,12 +20,20 @@ func AIEcho(ctx context.Context, q url.Values) (interface{}, error) {
 	arg0, arg1, arg2, arg3, arg4 := q.Get("arg0"), q.Get("arg1"), q.Get("arg2"), q.Get("arg3"), q.Get("arg4")
 	ol.Tf(ctx, "AI echo key=%v, args=%v", key, []string{arg0, arg1, arg2, arg3, arg4})
 
+	fnArg0 := func(pfn func(context.Context, string) string, ctx context.Context, arg0 string) []string {
+		rr := []string{}
+		for _, arg0 := range strings.Split(arg0, ",") {
+			rr = append(rr, fmt.Sprintf("**%v:** %v", arg0, pfn(ctx, arg0)))
+		}
+		return rr
+	}
+
 	var rr []string
 	switch key {
 	case "depends_env":
-		rr = DependsEnv(ctx, arg0)
+		rr = fnArg0(DependsEnv, ctx, arg0)
 	case "av_codecs":
-		rr = AVCodec(ctx, arg0)
+		rr = fnArg0(AVCodec, ctx, arg0)
 	default:
 		return UnknownKnowledge, nil
 	}
@@ -53,35 +61,39 @@ func AIEcho(ctx context.Context, q url.Values) (interface{}, error) {
 }
 
 // 实体：AVCodec
-func AVCodec(ctx context.Context, env string) []string {
+func AVCodec(ctx context.Context, env string) string {
 	switch env {
+	case "H.264", "AAC":
+		return "很好，各种浏览器和平台都支持"
+	case "H.263", "SPEEX", "PCM":
+		return "不支持，[太老的编码格式](https://github.com/ossrs/srs/blob/b4870a6d6f94ad26c7cc9c7fb39a4246180b5864/trunk/src/kernel/srs_kernel_codec.hpp#L35)，建议用[FFMPEG](https://github.com/ossrs/srs/wiki/v1_CN_SampleFFMPEG)转码为h.264"
+	case "H.265":
+		return "不支持，浏览器支持得不好，参考[#465](https://github.com/ossrs/srs/issues/465#issuecomment-562794207)"
+	case "AV1":
+		return "不支持，是未来的趋势，参考[#1070](https://github.com/ossrs/srs/issues/1070#issuecomment-562794926)"
+	case "MP3":
+		return "部分支持，不推荐，参考[#301](https://github.com/ossrs/srs/issues/301)和[#296](https://github.com/ossrs/srs/issues/296)"
+	case "Opus":
+		return "不支持，是WebRTC的音频编码，参考[#307](https://github.com/ossrs/srs/issues/307)"
 	default:
-		return nil
+		return NotSure
 	}
 }
 
 // 实体：依赖环境
-func DependsEnv(ctx context.Context, env string) []string {
-	fn := func(env string) string {
-		switch env {
-		case "CentOS", "x86-64":
-			return "很好，官方支持，建议直接用docker运行，参考[这里](https://github.com/ossrs/srs-docker/tree/srs3)"
-		case "Linux", "Unix":
-			return "可以，建议用docker[编译调试](https://github.com/ossrs/srs-docker/tree/dev)和[运行](https://github.com/ossrs/srs-docker/tree/srs3)"
-		case "ARM":
-			return "可以，需要替换ST(state-threads)，参考[这里](https://github.com/ossrs/state-threads/tree/srs#usage)"
-		case "Windows":
-			return "不支持，不过可以用docker运行，参考[这里](https://github.com/ossrs/srs/wiki/v1_CN_WindowsSRS)"
-		case "thingOS":
-			return "不支持"
-		default:
-			return NotSure
-		}
+func DependsEnv(ctx context.Context, env string) string {
+	switch env {
+	case "CentOS", "x86-64":
+		return "很好，官方支持，建议直接用docker运行，参考[这里](https://github.com/ossrs/srs-docker/tree/srs3)"
+	case "Linux", "Unix":
+		return "可以，建议用docker[编译调试](https://github.com/ossrs/srs-docker/tree/dev)和[运行](https://github.com/ossrs/srs-docker/tree/srs3)"
+	case "ARM":
+		return "可以，需要替换ST(state-threads)，参考[这里](https://github.com/ossrs/state-threads/tree/srs#usage)"
+	case "Windows":
+		return "不支持，不过可以用docker运行，参考[这里](https://github.com/ossrs/srs/wiki/v1_CN_WindowsSRS)"
+	case "thingOS":
+		return "不支持"
+	default:
+		return NotSure
 	}
-
-	rr := []string{}
-	for _, env := range strings.Split(env, ",") {
-		rr = append(rr, fmt.Sprintf("**%v:** %v", env, fn(env)))
-	}
-	return rr
 }
